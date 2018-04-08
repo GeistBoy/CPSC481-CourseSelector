28151091969c1ff994fe187fceb16c1f082e60ef
atheng <atheng@ucalgary.ca>
2018-04-08 13:25:53 -0600

    start on ReviewPanel.xaxml

--- CPSC481-A5/MainWindow.xaml.cs
+++ CPSC481-A5/MainWindow.xaml.cs
@@ -60,6 +60,12 @@ namespace CPSC481_A5
                 CourseControl.PreReqLabel.Text = c.PreReqToString();
                 CourseControl.Star.RatingValue = 3;
 
+                ReviewPanel review = new ReviewPanel();
+                CourseControl.CommentsStackPanel.Children.Add(review);
+                CourseControl.CommentsStackPanel.Children.Add(new ReviewPanel());
+                CourseControl.CommentsStackPanel.Children.Add(new ReviewPanel());
+
+
             }
 
         }
